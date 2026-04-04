a275ef3572cecbf007681dada0b9f205db704610
VictorPedroza <victor.pedroza@protonmail.com>
2026-04-04 14:42:12 -0300

    🐛 fix:(components): Updade import and export icons on header

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/layouts/header/header-component/header-component';
 import { AppRoutingModule } from "../app-routing-module";
-import { LucideHouse, LucideX } from '@lucide/angular';
+import { LucideMenu, LucideX } from '@lucide/angular';
 
 @NgModule({
   declarations: [
     HeaderComponent
   ],
-  imports: [CommonModule, AppRoutingModule, LucideHouse, LucideX],
-  exports: [HeaderComponent, LucideHouse, LucideX],
+  imports: [CommonModule, AppRoutingModule, LucideMenu, LucideX],
+  exports: [HeaderComponent, LucideMenu, LucideX],
 })
 export class SharedModule {}
