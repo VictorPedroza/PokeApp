767b99f05dbf4507467f505cecfda29e363da599
VictorPedroza <victor.pedroza@protonmail.com>
2026-04-03 21:34:59 -0300

    🎨 style:(components): Update HeaderComponent and add css on component

--- src/app/shared/components/layouts/header/header-component/header-component.ts
+++ src/app/shared/components/layouts/header/header-component/header-component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header-component',
   standalone: false,
-  templateUrl: './header-component.html'
+  templateUrl: './header-component.html',
+  styleUrl: './header-component.css'
 })
 export class HeaderComponent {
   // Estado do menu mobile
