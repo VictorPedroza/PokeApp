1aae86354522986c538ff868afe802eedfbc78df
VictorPedroza <victor.pedroza@protonmail.com>
2026-04-04 14:09:47 -0300

    ✨ feat:(module): Add shared module with icons

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/layouts/header/header-component/header-component';
 import { AppRoutingModule } from "../app-routing-module";
+import { LucideHouse, LucideX } from '@lucide/angular';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [CommonModule, AppRoutingModule],
-  exports: [HeaderComponent],
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [CommonModule, AppRoutingModule, LucideHouse, LucideX],
+  exports: [HeaderComponent, LucideHouse, LucideX],
 })
 export class SharedModule {}
