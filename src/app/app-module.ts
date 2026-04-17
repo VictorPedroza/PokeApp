9b68540a8d23831cff0e38508a935b94bd6cf86b
VictorPedroza <victor.pedroza@protonmail.com>
2026-04-17 19:48:18 -0300

    ✨ feat:(quiz): Init quiz module with routing

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -7,14 +7,14 @@ import { App } from './app';
 
 import { SharedModule } from './shared/shared-module';
 import { PokedexModule } from './pages/pokedex/pokedex-module';
+import { QuizModule } from './pages/quiz/quiz-module';
 
 import { HomeComponent } from './pages/home/home-component/home-component';
 import { TeamBuilderComponent } from './pages/team-builder/team-builder-component/team-builder-component';
-import { QuizComponent } from './pages/quiz/quiz-component/quiz-component';
 
 @NgModule({
-  declarations: [App, HomeComponent, TeamBuilderComponent, QuizComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, SharedModule, PokedexModule],
+  declarations: [App, HomeComponent, TeamBuilderComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, SharedModule, PokedexModule, QuizModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home-component/home-component';
-import { QuizComponent } from './pages/quiz/quiz-component/quiz-component';
 import { TeamBuilderComponent } from './pages/team-builder/team-builder-component/team-builder-component';
 
 const routes: Routes = [
@@ -11,7 +10,10 @@ const routes: Routes = [
     path: 'pokedex',
     loadChildren: () => import('./pages/pokedex/pokedex-module').then((m) => m.PokedexModule),
   },
-  { path: 'quiz', component: QuizComponent },
+  {
+    path: 'quiz',
+    loadChildren: () => import('./pages/quiz/quiz-module').then((m) => m.QuizModule),
+  },
   { path: 'team-builder', component: TeamBuilderComponent },
 ];
 
