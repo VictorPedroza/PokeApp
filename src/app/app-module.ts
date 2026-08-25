633251638de4a2673d1f5254596b6ea635093c2f
VictorPedroza <victor.pedroza@protonmail.com>
2026-08-25 18:34:59 -0300

    🧹 cleanup:(v2): Limpeza de código para implantação de uma nova versão da aplicação

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,19 +1,13 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
-import { SharedModule } from './shared/shared-module';
-import { PokedexModule } from './pages/pokedex/pokedex-module';
-import { QuizModule } from './pages/quiz/quiz-module';
 
-import { HomeComponent } from './pages/home/home-component/home-component';
-import { TeamBuilderModule } from './pages/team-builder/team-builder-module';
 @NgModule({
-  declarations: [App, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, SharedModule, PokedexModule, QuizModule, TeamBuilderModule],
+  declarations: [App],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,24 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home-component/home-component';
-
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'pokedex',
-    loadChildren: () => import('./pages/pokedex/pokedex-module').then((m) => m.PokedexModule),
-  },
-  {
-    path: 'quiz',
-    loadChildren: () => import('./pages/quiz/quiz-module').then((m) => m.QuizModule),
-  },
-  {
-    path: 'team-builder',
-    loadChildren: () =>
-      import('./pages/team-builder/team-builder-module').then((m) => m.TeamBuilderModule),
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
