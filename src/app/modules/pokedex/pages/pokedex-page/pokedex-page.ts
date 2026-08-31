90202b5fd2f39668c3c219f8516facc9a9601083
VictorPedroza <victor.pedroza@protonmail.com>
2026-08-31 20:43:45 -0300

    ✨ feat:(pokemon): Atualiza página inicializandos a busca dos pokemons

--- src/app/modules/pokedex/pages/pokedex-page/pokedex-page.ts
+++ src/app/modules/pokedex/pages/pokedex-page/pokedex-page.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { PokemonService } from '../../service/pokemon-service';
+import { Pokemon } from '../../../../shared/constants/pokemon/pokemon';
 
 @Component({
   selector: 'app-pokedex-page',
   standalone: false,
-  templateUrl: './pokedex-page.html'
+  templateUrl: './pokedex-page.html',
+  styleUrl: './pokedex-page.css',
 })
-export class PokedexPage {}
+export class PokedexPage implements OnInit {
+  pokemons: Pokemon[] = [];
+
+  constructor(private api: PokemonService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.api.buscarPokemons().subscribe({
+      next: (response) => {
+        console.log(response);
+        this.pokemons = response;
+        this.cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error('Error: ', error);
+      },
+    });
+  }
+}
