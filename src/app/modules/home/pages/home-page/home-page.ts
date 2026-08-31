fa2322767308c7696492cc4dc4c8e2993e502305
VictorPedroza <victor.pedroza@protonmail.com>
2026-08-31 16:24:26 -0300

    ✨ feat:(home): Inicializa resumo sobre dados do usuário

--- src/app/modules/home/pages/home-page/home-page.ts
+++ src/app/modules/home/pages/home-page/home-page.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home-page',
   standalone: false,
-  templateUrl: './home-page.html'
+  templateUrl: './home-page.html',
 })
 export class HomePage {}
